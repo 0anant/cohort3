b8de28f27ce902b5ed358e3bd956cd58051eb6c2
0anant <bigbluemagic06@gmail.com>
2025-06-12 22:09:02 +0530

    another i/o

--- demo.js
+++ demo.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
-const contents =  fs.readFileSync("a.txt","utf-8");
-console.log(contents);
+fs.readFileSync("a.txt","utf-8",function(err, contents){
+    console.log(contents);
+});
 
-const contents2 =  fs.readFileSync("b.txt","utf-8");
-console.log(contents2);
+
+fs.readFileSync("b.txt","utf-8", function(err, contents2){
+    console.log(contents2); 
+});
+
+fs.readFileSync("b.txt","utf-8", function(err, contents3){
+    console.log(contents3); 
+});
