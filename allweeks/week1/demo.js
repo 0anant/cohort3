2fafe6abd875fb4791e78bbf9a577ff852ae964e
0anant <bigbluemagic06@gmail.com>
2025-06-13 12:58:08 +0530

    week1

--- allweeks/week1/demo.js
+++ allweeks/week1/demo.js
@@ -14,18 +14,28 @@
 // });                              //----it's running synchronously
 
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.readFile("a.txt","utf-8",function(err, contents){
-    console.log(contents);
-});
+// fs.readFile("a.txt","utf-8",function(err, contents){
+//     console.log(contents);
+// });
 
 
-fs.readFile("b.txt","utf-8", function(err, contents2){
-    console.log(contents2); 
-});
+// fs.readFile("b.txt","utf-8", function(err, contents2){
+//     console.log(contents2); 
+// });
 
-fs.readFile("b.txt","utf-8", function(err, contents3){
-    console.log(contents3); 
-});                             // ----it's running asynchronously
+// fs.readFile("b.txt","utf-8", function(err, contents3){
+//     console.log(contents3); 
+// });                             // ----it's running asynchronously
 
+const fs = require("fs");
+
+function print(err,data){
+    console.log(data);
+}
+
+fs.readFile("a.txt", "utf-8", print);
+fs.readFile("b.txt", "utf-8", print);
+
+console.log("Done!");
