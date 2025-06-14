1319c1d6370e925e820604e02bcb29fd08a2bc46
0anant <bigbluemagic06@gmail.com>
2025-06-14 19:16:35 +0530

    DOM manipulation

---
+++ allweeks/week3/script.js
@@ -0,0 +1,17 @@
+// let ctr = 0;
+// function callback(){
+//     console.log(ctr);
+//     ctr = ctr +1;
+// }
+
+// setTimeout(callback,1000);  ---runs only once
+
+
+// let ctr =0 
+// function callback(){
+//     console.log(ctr);
+//     ctr = ctr + 1;
+// }
+
+// setInterval(callback ,1000); ---runs after every 1 sec infinite loop
+
