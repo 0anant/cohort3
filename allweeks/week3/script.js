79f2236cc1514f5316a946c39c2fde1be42eee10
0anant <bigbluemagic06@gmail.com>
2025-06-14 21:01:25 +0530

    all html files changed

--- allweeks/week3/script.js
+++ allweeks/week3/script.js
@@ -15,3 +15,9 @@
 
 // setInterval(callback ,1000); ---runs after every 1 sec infinite loop
 
+// let ctr = 0;
+// function callback(){
+//     document.querySelectorAll("h4")[1].innerHTML  = ctr;
+//     ctr = ctr +1;
+// }
+// setInterval(callback,1000);
