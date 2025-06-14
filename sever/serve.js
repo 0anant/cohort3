4456d8c0706f6726e7f2d5ad1ee34a52a76a3d35
0anant <bigbluemagic06@gmail.com>
2025-06-14 19:19:09 +0530

    building my own HTTP server

--- sever/serve.js
+++ sever/serve.js
@@ -1,9 +1,11 @@
-const http = require("http");
-const fs = require("fs");
+const http = require(`http`);
+const fs = require(`fs`);
 
-http.createServer(function(req, res)
-{
-    res.write("hello world");
-    res.end();
-
-}).listen(8000);
+http.createServer((req, res) =>
+{  
+    fs.readFile('/Users/nisha/Desktop/cohort 3/demo pgm/sever/index.html' ,(err,data) =>{
+    res.writeHead(200 , {'Content-Type':'text/html'});
+    res.write(data)
+    res.end()
+    })
+}).listen(8000)
