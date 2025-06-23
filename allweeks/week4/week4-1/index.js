cb246fddb965ce58c6be432653c519744b19e5f2
0anant <bigbluemagic06@gmail.com>
2025-06-23 21:25:35 +0530

    another http server

---
+++ allweeks/week4/week4-1/index.js
@@ -0,0 +1,24 @@
+//creating a http server
+//express
+//node default library => no
+
+const express = require("express");
+
+const app = express();
+
+function sum(n){
+    let ans = 0;
+    for (let i =0; i<=n; i++){
+        ans = ans +i;
+    }
+    return ans;
+}
+
+//route get
+app.get('/', (req, res) => {
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send('Hi your ans is' + ans);
+})
+
+app.listen(3000);
