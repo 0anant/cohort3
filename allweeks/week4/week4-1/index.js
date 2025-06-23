8539ae7a6ef1efd02326a21959365c145f6fce60
0anant <bigbluemagic06@gmail.com>
2025-06-23 22:31:45 +0530

    added changes

--- allweeks/week4/week4-1/index.js
+++ allweeks/week4/week4-1/index.js
@@ -6,6 +6,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 var users = [{
     name: 'jhon',
     kidneys: [{
@@ -37,17 +39,38 @@ app.get('/', (req, res) => {
 })
 
 //route put
-app.put('/put', (req, res) => {
+app.put('/', (req, res) => {
+    for (let i =0; i<users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({})
 
 })
 
 //route post
-app.post('/post', (req, res) => {
+app.post('/', (req, res) => {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "done"
+    })
 
 })
 
 //route delete
-app.delete('/delete', (req, res) => {
+app.delete('/', (req, res) => {
+    const newKidneys = []
+    for (let i=0; i<users[0].kidneys.length; i++){
+        if(users[0].kidneys[i].healthy){
+            newKidneys.push({
+                healthy: true
+            })
+        }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({msg: "done!"})
 
 })
 
