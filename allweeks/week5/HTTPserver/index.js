4786ffa3c9163598eb46af1e5b52da9b9daeeb4b
0anant <bigbluemagic06@gmail.com>
2025-06-24 10:43:59 +0530

    creating an HTTP server

---
+++ allweeks/week5/HTTPserver/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+
+const app = express();
+
+app.get('/multiply', (req, res) => {
+    const a = req.query.a
+    const b = req.query.b
+
+    res.json({
+        ans: a*b
+    })
+})
+
+app.get('/add', (req, res) => {
+    const a = req.query.a
+    const b = req.query.b
+
+    res.json({
+        ans: a+b
+    })
+})
+
+app.get('/subtract', (req, res) => {
+    const a = req.query.a
+    const b = req.query.b
+
+    res.json({
+        ans: a-b
+    })
+})
+
+app.get('/divide', (req, res) => {
+    const a = req.query.a
+    const b = req.query.b
+
+    res.json({
+        ans: a/b
+    })
+})
+
+app.listen(3000)
