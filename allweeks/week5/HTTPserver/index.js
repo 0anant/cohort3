b4c2e6f0a392c949d2bdbb9137b33de17a109f2b
0anant <bigbluemagic06@gmail.com>
2025-06-24 20:35:21 +0530

    changed file

--- allweeks/week5/HTTPserver/index.js
+++ allweeks/week5/HTTPserver/index.js
@@ -2,40 +2,66 @@ const express = require("express")
 
 const app = express();
 
-app.get('/multiply', (req, res) => {
-    const a = req.query.a
-    const b = req.query.b
+
+//better routing ,add middlewares and databases
+//write a middlewares that logs the method , timestamp and the url
+// let numOfReq = 0;
+
+// function noOfReq(req, res, next){
+//     numOfReq = numOfReq + 1;
+//     console.log("total no requests = " + numOfReq)
+//     next();
+// }
+
+// function middlewares(req, res, next){
+//     console.log("method request is =" + req.method);
+//     console.log("url is =" + req.url);
+//     console.log(new Date());
+
+//     next();
+// }
+
+// app.use(middlewares);
+// app.use(noOfReq);
+app.use(express.json())
+app.post('/multiply', (req, res) => {
+    console.log(req.body);
+    const a = req.body.a
+    const b = req.body.b
 
     res.json({
         ans: a*b
     })
 })
 
-app.get('/add', (req, res) => {
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b)
+// app.get('/add', (req, res) => {
+     
+//     const a = parseInt(req.query.a)
+//     const b = parseInt(req.query.b)
 
-    res.json({
-        ans: a+b
-    })
-})
+//     res.json({
+//         ans: a+b
+//     })
+// })
 
-app.get('/subtract', (req, res) => {
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b)
+// app.get('/subtract', (req, res) => {
+    
+//     const a = parseInt(req.query.a)
+//     const b = parseInt(req.query.b)
 
-    res.json({
-        ans: a-b
-    })
-})
+//     res.json({
+//         ans: a-b
+//     })
+// })
 
-app.get('/divide', (req, res) => {
-    const a = req.query.a
-    const b = req.query.b
+// app.get('/divide', (req, res) => {
+    
+//     const a = req.query.a
+//     const b = req.query.b
 
-    res.json({
-        ans: a/b
-    })
-})
+//     res.json({
+//         ans: a/b
+//     })
+// })
 
 app.listen(3000)
