43cfa28054bc14c51bd4df82968bdbe120197ac7
0anant <bigbluemagic06@gmail.com>
2025-06-27 16:25:09 +0530

    changes

--- allweeks/week8/index.js
+++ allweeks/week8/index.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {UserModel, AdminModel, PurchaseModel, CourseMode } = require("./db");
 
 mongoose.connect("");
 const app = express();
 
 app.use(express.json());
 
-app.post("/usersignup", auth, (req, res) => {
+app.post("/usersignup", auth, async (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    const course = req.body.course
 
+    UserModel.create({
+        email: email,
+        password:password,
+        name:name,
+        course:course
+    })
+
+    res.json({
+        message:"you are signed up"
+    })
+ 
 })
 
 app.post("/usersignin", auth,  (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    
 
 })
 
